3fb0b49f96875d1b3b8734122d9dc5667ac9fe14
oussama <64930789+oussama-fellag@users.noreply.github.com>
2024-01-15 12:25:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const display = document.querySelector('.display');
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    display.value += operator;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
